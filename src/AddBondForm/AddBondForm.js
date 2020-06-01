e9bccd943ae9c468c2152e8cacf8310dabc0ed30
Caleb King <caleb.king536@gmail.com>
2020-06-01 10:21:49 -0600

    Add components LandingPage and AddBondForm

---
+++ src/AddBondForm/AddBondForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './AddBondForm.css';
+
+function AddBondForm() {
+  return (
+    <main role="main">
+      <header>
+        <h1>New Bond</h1>
+      </header>
+      <section>
+        <form id="create-bond">
+          <div className="form-section">
+            <label for="bond-name">Name</label>
+            <input type="text" name="bond-name" required />
+          </div>
+          <div className="form-section">
+            <label for="bond-group">Group</label>
+            <select name="bond-group" id="bond-group">
+              <option value="no-group">None</option>
+              <option value="close-family">Close Family</option>
+              <option value="family">Family</option>
+              <option value="close-friends">Close Friends</option>
+              <option value="friends">Friends</option>
+              <option value="Coworkers">Coworkers</option>
+            </select>
+            <br />
+            <a href="url" className="add-group-link">+ Add a Group</a>
+          </div>
+          <div className="form-section">
+            <p className="bond-birthday">Birthday</p>
+            <input type="number" name="date-month" placeholder="01" min="1" max="12" /> .
+            <input type="number" name="date-day" className="date-day"  placeholder="01" min="1" max="31" /> .
+            <input type="number" name="date-year" className="date-year" placeholder="1990" />
+          </div>
+          <div className="form-section">
+            <label for="bond-notes">Notes</label>
+            <textarea name="bond-notes" rows="15" placeholder="Future discussion topics, gift ideas, significant details..."></textarea>
+          </div>
+
+          <button type="submit">Submit</button>
+          <button>Cancel</button>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default AddBondForm;
---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LandingPage from '../LandingPage/LandingPage';
+import AddBondForm from '../AddBondForm/AddBondForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AddBondForm />
+    </div>
+  );
+}
+
+export default App;
