e754793cfe2b437cb5ba49b2218cf25eeb748163
Caleb King <caleb.king536@gmail.com>
2020-06-20 16:39:01 -0600

    add icons and underlining for NavBar - conditional on pathname

--- src/common/NavBar/NavBar.js
+++ src/common/NavBar/NavBar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import { FaLink, FaExchangeAlt } from "react-icons/fa";
+import { useLocation } from 'react-router-dom';
 
 function NavBar() {
+  const currentLocation = useLocation().pathname;
+  const isOnBondsPage = currentLocation.includes('bonds');
+  const isOnInteractionsPage = currentLocation.includes('interactions');
+  
   return (
     <nav role="navigation">
     <div className="nav-right">
       <ul>
-        <li><Link to="/bonds">Bonds</Link></li>
-        <li><Link to="/interactions">Interactions</Link></li>
+        <li>
+          <Link to="/bonds" className={isOnBondsPage && 'selected'}><FaLink size={15}/>     Bonds</Link>
+        </li>
+        <li>
+          <Link to="/interactions" className={isOnInteractionsPage && 'selected'}><FaExchangeAlt  size={15}/>     Interactions</Link>
+        </li>
       </ul>
     </div>
   </nav>
