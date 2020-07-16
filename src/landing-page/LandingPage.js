0ffb2e2b4e68f2df42d2295383a77102245f35a3
Caleb King <caleb.king536@gmail.com>
2020-07-16 19:08:37 -0400

    create and polish landing page

--- src/landing-page/LandingPage.js
+++ src/landing-page/LandingPage.js
@@ -3,46 +3,65 @@ import { Link } from 'react-router-dom';
 import './LandingPage.css';
 
 function LandingPage() {
+  
+  function handleChevronClicked(e) {
+    e.currentTarget.blur();
+    document.querySelector('.how-it-works-container').scrollIntoView({ 
+      behavior: 'smooth' 
+    });
+  }
+  
   return (
-    <main role="main">
-      <header role="banner">
-        <h1>EverBond</h1>
-        <h2>Sustain and grow the relationships that matter to you.</h2>
+    <main className="landing-page">
+      <header>
+        <div className="main-header-container">
+          <div className="headline-container">
+            <p className="headline">Life is busy.</p>
+            <p className="headline">Friends and family matter.</p>
+          </div>
+          <div className="value-proposition-container">
+            <p className="value-proposition">Let EverBond help you <span>SUSTAIN</span> and <span>GROW</span> your personal relationships.</p>
+          </div>
+          <Link to="/interactions" className="demo-button-light">
+            Explore EverBond
+          </Link>
+        </div>
+        <button className="view-more-below" onClick={handleChevronClicked}>
+          <i className="fas fa-chevron-down"></i>
+        </button>
       </header>
-      <section>
-        <header>
-            <h3>Monitor relational health</h3>
-        </header>
-        <p>[<em>placeholder for screenshot of EverBond interface</em>]</p>
-        <p>In the bustle of daily living it is all too common to not give the proper attention to the people you care about. This accidental neglect causes a slow distancing of once cherished relationships. With EverBond as your assistant, you can be more proactive and intentional with your family and friends, preventing relational decay.</p>
-      </section>
-      <section>
-        <header>
-            <h3>Record your interactions</h3>
-        </header>
-        <p>[<em>placeholder for screenshot of EverBond interface</em>]</p>
-        <p>The key to healthy personal relationships is to have meaningful points of connection. These intentional interactions build common experiences and relational capital. EverBond provides you with a simple and fast way to record these interactions so that you can keep track of the quantity and quality of these touchpoints.</p>
-      </section>
-      <section>
-        <header>
-            <h3>Add events</h3>
-        </header>
-        <p>[<em>placeholder for screenshot of EverBond interface</em>]</p>
-        <p>Adding events such as birthdays, anniversaries, and graduations to EverBond will keep you from forgetting those special moments in your family and friend's lives.</p>
-      </section>
-      <section>
-        <header>
-            <h3>Set intentions</h3>
-        </header>
-        <p>[<em>placeholder for screenshot of EverBond interface</em>]</p>
-        <p>With EverBond, you can make a commitment to invest more in relationships and track your follow through.</p>
-      </section>
-      <section>
-        <header>
-            <h3>Try EverBond out for yourself!</h3>
-        </header>
-        <Link to="/interactions">Demo</Link>
-      </section>
+      <div className="how-it-works-container">
+        <section className="how-it-works">
+          <h2>How EverBond Works</h2>
+          <div className="explanation-container">
+            <section className="record-interaction">
+              <div className="explanation-text-content">
+                <h3><i className="fas fa-edit"></i>Record your interactions</h3>
+                <p>Healthy relationships require meaningful points of connection. These shared moments build relational capital.</p> 
+                <p>Everbond helps you keep track of the quantity and quality of these interactions by providing a simple way to record and review them.</p>
+              </div>
+              <div className="screenshot-container">
+                <video src="videos/adding-interaction-screen-recording.mp4" className="how-to-video" autoPlay="1" loop="1" preload="auto"></video>
+              </div>
+            </section>
+            <section className="monitor-health">
+              <div className="explanation-text-content">
+                <h3><i className="fas fa-heartbeat"></i>Monitor relational health</h3>
+                <p>You mean well... but life can get so busy. Good intentions aren't enough to prevent the slow distancing of once cherished relationships.</p> 
+                <p>With EverBond as your assistant, you can be more proactive and intentional with your family and friends, preventing relational decay.</p>
+              </div>
+              <div className="screenshot-container">
+                <video src="videos/monitoring-bond-screen-recording.mp4" className="how-to-video" autoPlay="1" loop="1" preload="auto"></video>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="final-demo-button">
+          <Link to="/interactions" className="demo-button-dark">
+            Explore EverBond
+          </Link>
+        </section>
+      </div>
     </main>
   );
 }
